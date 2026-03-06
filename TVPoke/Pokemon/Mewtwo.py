a59fa1e4d5a118d642f425d28eae5e0443573b10
samanthaj24 <samanthgju@icloud.com>
2026-03-06 11:27:19 -0700

    Changed the attacks

--- TVPoke/Pokemon/Mewtwo.py
+++ TVPoke/Pokemon/Mewtwo.py
@@ -4,9 +4,9 @@ from TVPoke.BaseClasses.Move import Move
 class Mewtwo(Psychic):
     def __init__(self):
         moves = [
-            Move("Tackle", "NORMAL", 40),
-            Move("Water Gun", "WATER", 40),
-            Move("Surf", "WATER", 80),
-            Move("Splash", "WATER", 0)
+            Move("Physco Cut", "PHYSCIC", 70),
+            Move("Aura Sphere", "FIGHTING", 80),
+            Move("Confusion", "PHYSCIC", 50),
+            Move("PsyBeam", "PHYSCIC", 65)
         ]
         super().__init__("MewTwo", 80, moves, "./TVPoke/Pokemon/imgs/Mewtwo.png")
